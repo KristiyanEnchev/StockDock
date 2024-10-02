355fc08f657acd80f4d5f99f7a69af6b8a2d8b78
Kiko <kristiqnenchevv@gmail.com>
2024-10-02 12:47:51 +0300

    Setup Swagger

---
+++ Server/API/Web/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace Web
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Startup
+    {
+        public static IServiceCollection AddWeb(this IServiceCollection services, IConfiguration config)
+        {
+            return services;
+        }
+    }
+}
