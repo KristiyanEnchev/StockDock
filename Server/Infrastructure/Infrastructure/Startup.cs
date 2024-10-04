86644ead4f93ba6f49bbb1be240df23c2f86fe73
Kiko <kristiqnenchevv@gmail.com>
2024-10-04 13:59:36 +0300

    Add Identity related services

---
+++ Server/Infrastructure/Infrastructure/Startup.cs
@@ -0,0 +1,44 @@
+﻿namespace Infrastructure
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Startup
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddServices()
+                .AddIdentity(configuration)
+                .AddConfigurations(configuration)
+                .AddCustomAuthentiation(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static async Task InitializeDatabase(this IServiceProvider services)
+        {
+            using var scope = services.CreateScope();
+        }
+
+        public static IServiceCollection AddIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddCustomAuthentiation(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+
+        private static IServiceCollection AddConfigurations(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
