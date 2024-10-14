c217a48e56883300f124f0e2cb9091e0ce208706
Kiko <kristiqnenchevv@gmail.com>
2024-10-14 13:56:52 +0300

    Generate token method

--- Server/Infrastructure/Infrastructure/Services/Identity/IdentityService.cs
+++ Server/Infrastructure/Infrastructure/Services/Identity/IdentityService.cs
@@ -13,6 +13,8 @@
 
     using Application.Interfaces;
 
+    using Models.User;
+
     internal class IdentityService
     {
         private const string InvalidErrorMessage = "Invalid credentials.";
@@ -43,105 +45,6 @@
             var tokenResult = new UserResponseModel(token, user.RefreshTokenExpiryTime, newRefreshToken);
 
             return tokenResult;
-        }
-
-        public async Task<Result<string>> Register(UserRegisterRequestModel userRequest)
-        {
-            var userExists = await userManager.FindByEmailAsync(userRequest.Email);
-
-            if (userExists != null)
-            {
-                return Result<string>.Failure("Email already in use.");
-            }
-
-            var user = new User()
-            {
-                FirstName = userRequest.FirstName,
-                LastName = userRequest.LastName,
-                Email = userRequest.Email,
-                UserName = userRequest.Email,
-                IsActive = true,
-                EmailConfirmed = true,
-                CreatedBy = "Registration",
-            };
-
-            var createUserResult = await userManager.CreateAsync(user, userRequest.Password);
-            if (!createUserResult.Succeeded)
-            {
-                var errors = createUserResult.Errors.Select(e => e.Description).ToList();
-                return Result<string>.Failure(errors);
-            }
-
-            var addToRoleResult = await userManager.AddToRoleAsync(user, "User");
-            if (!addToRoleResult.Succeeded)
-            {
-                var errors = addToRoleResult.Errors.Select(e => e.Description).ToList();
-                return Result<string>.Failure(errors);
-            }
-
-            return Result<string>.SuccessResult("Succesfull Registration !");
-        }
-
-        public async Task<Result<UserResponseModel>> Login(UserRequestModel userRequest)
-        {
-            var email = userRequest.Email.Trim().Normalize();
-            var user = await userManager.FindByEmailAsync(email);
-            if (user == null || !user.IsActive)
-            {
-                return Result<UserResponseModel>.Failure(new List<string> { InvalidErrorMessage });
-            }
-
-            if (!user.EmailConfirmed || await userManager.IsLockedOutAsync(user))
-            {
-                return Result<UserResponseModel>.Failure(new List<string> { "Account is not accessible at the moment." });
-            }
-
-            SignInResult signInResult = await signInManager.PasswordSignInAsync(user, userRequest.Password, false, lockoutOnFailure: true);
-
-            if (!signInResult.Succeeded)
-            {
-                return Result<UserResponseModel>.Failure(new List<string> { InvalidErrorMessage });
-            }
-
-            var tokenResult = await GenerateToken(user);
-
-            return Result<UserResponseModel>.SuccessResult(tokenResult);
-        }
-
-        public async Task<Result<UserResponseModel>> RefreshTokenAsync(UserRefreshModel request)
-        {
-            var user = await userManager.FindByEmailAsync(request.Email);
-
-            if (user == null)
-            {
-                return Result<UserResponseModel>.Failure(new List<string> { InvalidErrorMessage });
-            }
-
-            string oldRefreshToken = await userManager.GetAuthenticationTokenAsync(user, "TodoSwitch", "RefreshToken");
-            bool isValid = await userManager.VerifyUserTokenAsync(user, "TodoSwitch", "RefreshToken", request.RefreshToken);
-
-            if (oldRefreshToken == null || !oldRefreshToken.Equals(request.RefreshToken) || !isValid)
-            {
-                return Result<UserResponseModel>.Failure(new List<string> { $"Your token is not valid." });
-            }
-
-            var tokenResult = await GenerateToken(user);
-
-            await signInManager.SignInAsync(user, false);
-
-            return Result<UserResponseModel>.SuccessResult(tokenResult);
-        }
-
-        public async Task<Result<string>> LogoutAsync(string userEmail)
-        {
-            var user = await userManager.FindByEmailAsync(userEmail);
-            if (user != null)
-            {
-                await userManager.RemoveAuthenticationTokenAsync(user, "TodoSwitch", "RefreshToken");
-            }
-            await signInManager.SignOutAsync();
-
-            return Result<string>.SuccessResult("Succesfull Logout !");
         }
     }
 }
