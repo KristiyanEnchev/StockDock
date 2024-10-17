6f4565bd15371698d356837b25a021d8b7b56249
Kiko <kristiqnenchevv@gmail.com>
2024-10-17 14:01:28 +0300

    Create Result extention method

---
+++ Server/API/Web/Extensions/ResultExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace Web.Extensions
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Shared;
+
+    public static class ResultExtensions
+    {
+        public static async Task<ActionResult> ToActionResult<TData>(this Task<Result<TData>> resultTask)
+        {
+            var result = await resultTask;
+
+            var response = new
+            {
+                success = result.Success,
+                data = result.Data,
+                errors = !result.Success ? result.Errors : null
+            };
+
+            if (!result.Success)
+            {
+                if (result.Errors != null)
+                {
+                    return new BadRequestObjectResult(new { Errors = response.errors });
+                }
+                return new BadRequestObjectResult("Unexpected Error occured");
+            }
+
+            else if (result.Data == null)
+            {
+                return new NoContentResult();
+            }
+
+            if (response.data is string)
+            {
+                return new OkObjectResult(new
+                {
+                    message = result.Data,
+                });
+            }
+
+            return new OkObjectResult(response.data);
+        }
+    }
+}
