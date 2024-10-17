3d6b6c3f18d7e5ed4afdb939589612d20d1e1b1c
Kiko <kristiqnenchevv@gmail.com>
2024-10-17 15:52:44 +0300

    Create Registration Controller

--- Server/API/Web/Controllers/Identity/IdentityController.cs
+++ Server/API/Web/Controllers/Identity/IdentityController.cs
@@ -1,7 +1,24 @@
 ﻿namespace Web.Controllers.Identity
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    using Swashbuckle.AspNetCore.Annotations;
+
+    using Shared;
+
+    using Application.Handlers.Identity.Commands.Register;
+
+    using Web.Extensions;
+
     public class IdentityController : ApiController
     {
-       
+        [AllowAnonymous]
+        [HttpPost(nameof(Register))]
+        [SwaggerOperation("Register a user.", "")]
+        public async Task<ActionResult<string>> Register(UserRegisterCommand request)
+        {
+            return await Mediator.Send(request).ToActionResult();
+        }
     }
 }
