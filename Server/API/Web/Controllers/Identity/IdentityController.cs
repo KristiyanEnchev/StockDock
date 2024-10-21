5a940a76133ee140df6a1c5adc0231e28071164b
Kiko <kristiqnenchevv@gmail.com>
2024-10-21 11:13:51 +0300

    Create Login Controller

--- Server/API/Web/Controllers/Identity/IdentityController.cs
+++ Server/API/Web/Controllers/Identity/IdentityController.cs
@@ -7,9 +7,12 @@
 
     using Shared;
 
+    using Application.Handlers.Identity.Commands.Login;
     using Application.Handlers.Identity.Commands.Register;
 
     using Web.Extensions;
+
+    using Models.User;
 
     public class IdentityController : ApiController
     {
@@ -17,6 +20,14 @@
         [HttpPost(nameof(Register))]
         [SwaggerOperation("Register a user.", "")]
         public async Task<ActionResult<string>> Register(UserRegisterCommand request)
+        {
+            return await Mediator.Send(request).ToActionResult();
+        }
+
+        [AllowAnonymous]
+        [HttpPost(nameof(Login))]
+        [SwaggerOperation("Request an access token using credentials.", "")]
+        public async Task<ActionResult<Result<UserResponseModel>>> Login(UserLoginCommand request)
         {
             return await Mediator.Send(request).ToActionResult();
         }
