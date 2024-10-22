76f406aacf078a0a2561a27bdd99527be99f19dc
Kiko <kristiqnenchevv@gmail.com>
2024-10-22 13:03:03 +0300

    Create Logout Handler

---
+++ Server/Core/Application/Handlers/Identity/Commands/Logout/UserLogoutCommand.cs
@@ -0,0 +1,37 @@
+﻿namespace Application.Handlers.Identity.Commands.Logout
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using MediatR;
+
+    using Models.User;
+
+    using Application.Interfaces;
+
+    using Shared;
+
+    public class UserLogoutCommand : UserLogoutModel, IRequest<Result<string>>
+    {
+        public UserLogoutCommand(string email)
+            : base(email)
+        {
+        }
+
+        public class UserLogoutCommandHandler : IRequestHandler<UserLogoutCommand, Result<string>>
+        {
+            private readonly IIdentityService identity;
+
+            public UserLogoutCommandHandler(IIdentityService identity)
+            {
+                this.identity = identity;
+            }
+
+            public async Task<Result<string>> Handle(UserLogoutCommand request, CancellationToken cancellationToken)
+            {
+                var result = await identity.LogoutAsync(request.Email);
+
+                return result;
+            }
+        }
+    }
+}
