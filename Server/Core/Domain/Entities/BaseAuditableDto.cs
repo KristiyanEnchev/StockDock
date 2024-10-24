2a883ae5da86ab13a89b2b5f958b6506afb02235
Kiko <kristiqnenchevv@gmail.com>
2024-10-24 14:54:15 +0300

    Apply Base Dto logic same way as the Base Entity

---
+++ Server/Core/Domain/Entities/BaseAuditableDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public abstract class BaseAuditableDto<TDto, TEntity> : BaseDto<TDto, TEntity>
+        where TDto : class, new()
+        where TEntity : BaseAuditableEntity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTimeOffset? CreatedDate { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTimeOffset? UpdatedDate { get; set; }
+    }
+}
---
+++ Server/Core/Domain/Entities/BaseDto.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.Entities
+{
+    using Mapster;
+
+    public abstract class BaseDto<TDto, TEntity> : IMapFrom<TEntity>
+        where TDto : class, new()
+        where TEntity : class
+    {
+        public virtual void Mapping(TypeAdapterConfig config)
+        {
+            config.NewConfig<TEntity, TDto>()
+                .IgnoreNullValues(true)
+                .PreserveReference(true)
+                .MaxDepth(3)
+                .Compile();
+
+            config.NewConfig<TDto, TEntity>()
+                .IgnoreNullValues(true)
+                .PreserveReference(true)
+                .MaxDepth(3)
+                .Compile();
+        }
+
+        public virtual void CustomizeMapping(TypeAdapterConfig config)
+        {
+        }
+
+        public TEntity ToEntity() => this.Adapt<TEntity>();
+        public TEntity ToEntity(TEntity entity) => (this as TDto).Adapt(entity);
+        public static TDto FromEntity(TEntity entity) => entity.Adapt<TDto>();
+    }
+}
