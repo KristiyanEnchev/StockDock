4ce7f8755686c48e5ccb6d658839fd5e60c91a66
Kiko <kristiqnenchevv@gmail.com>
2024-10-28 13:08:42 +0200

    Create repository interface

---
+++ Server/Infrastructure/Persistence/Repositories/IRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Persistence.Repositories
+{
+    using System.Linq.Expressions;
+
+    using Domain.Entities;
+
+    public interface IRepository<TEntity> where TEntity : BaseAuditableEntity
+    {
+        Task<TDto?> GetByIdAsync<TDto>(string id, CancellationToken cancellationToken = default) where TDto : class;
+        Task<TDto?> FirstOrDefaultAsync<TDto>(
+            Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default) where TDto : class;
+        Task<IReadOnlyList<TDto>> GetAllAsync<TDto>(CancellationToken cancellationToken = default) where TDto : class;
+        Task<IReadOnlyList<TDto>> FindAsync<TDto>(
+            Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default) where TDto : class;
+
+        Task<TEntity> AddAsync(TEntity entity, string userId, CancellationToken cancellationToken = default);
+        Task UpdateAsync(TEntity entity, string userId, CancellationToken cancellationToken = default);
+        Task DeleteAsync(TEntity entity, string userId, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        IQueryable<TEntity> AsNoTracking();
+        IQueryable<TEntity> AsTracking();
+    }
+}
