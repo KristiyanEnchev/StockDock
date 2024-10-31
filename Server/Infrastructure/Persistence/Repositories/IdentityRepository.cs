0ade59acdead67915c826ce13ef37f82dd46a4a2
Kiko <kristiqnenchevv@gmail.com>
2024-10-31 14:31:10 +0200

    Add constructor and inject dependencies in the repository

--- Server/Infrastructure/Persistence/Repositories/IdentityRepository.cs
+++ Server/Infrastructure/Persistence/Repositories/IdentityRepository.cs
@@ -14,6 +14,19 @@
     public class IdentityRepository<TEntity> : IIdentityRepository<TEntity>
         where TEntity : BaseIdentityAuditableEntity
     {
-       
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        private readonly ILogger<IdentityRepository<TEntity>> _logger;
+
+        public IdentityRepository(
+            ApplicationDbContext context,
+            ILogger<IdentityRepository<TEntity>> logger)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _dbSet = context.Set<TEntity>();
+        }
+
+
     }
 }
