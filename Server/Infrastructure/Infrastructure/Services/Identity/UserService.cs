1af3fc837715834481747e4b5d2103ba2f3191b3
Kiko <kristiqnenchevv@gmail.com>
2024-11-04 13:28:29 +0200

    Create initial UserService and add get methods working with the repository

---
+++ Server/Infrastructure/Infrastructure/Services/Identity/UserService.cs
@@ -0,0 +1,63 @@
+﻿namespace Infrastructure.Services.Identity
+{
+    using Microsoft.Extensions.Logging;
+    using Microsoft.AspNetCore.Identity;
+
+    using Mapster;
+
+    using Application.Interfaces;
+
+    using Domain.Entities;
+
+    using Models.User;
+
+    using Shared;
+    using Shared.Exceptions;
+    using Shared.Interfaces;
+
+    public class UserService : IUserService
+    {
+        private readonly IIdentityRepository<User> _userRepository;
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<UserService> _logger;
+
+        public UserService(
+            IIdentityRepository<User> userRepository,
+            UserManager<User> userManager,
+            ILogger<UserService> logger)
+        {
+            _userRepository = userRepository;
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public async Task<UserDto> GetByIdAsync(string id)
+        {
+            try
+            {
+                return await _userRepository.GetByIdAsync<UserDto>(id)
+                    ?? throw new CustomException($"User with ID {id} not found.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while getting user by id {Id}", id);
+                throw;
+            }
+        }
+
+        public async Task<IReadOnlyList<UserDto>> GetAllAsync()
+        {
+            try
+            {
+                return await _userRepository.GetAllAsync<UserDto>();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while getting all users");
+                throw;
+            }
+        }
+
+       
+    }
+}
