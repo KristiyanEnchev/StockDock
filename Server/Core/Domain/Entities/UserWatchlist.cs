180c077c15e349ed25fb92715690b7e37d8adfa1
Kiko <kristiqnenchevv@gmail.com>
2024-11-20 13:09:21 +0200

    Create Stock history and Watchlist entities

---
+++ Server/Core/Domain/Entities/UserWatchlist.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class UserWatchlist : BaseAuditableEntity
+    {
+        public string UserId { get; set; } = string.Empty;
+        public User User { get; set; } = null!;
+        public string StockId { get; set; } = string.Empty;
+        public Stock Stock { get; set; } = null!;
+        public decimal? AlertAbove { get; set; }
+        public decimal? AlertBelow { get; set; }
+    }
+}
