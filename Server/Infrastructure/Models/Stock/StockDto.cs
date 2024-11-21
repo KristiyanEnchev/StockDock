91e0cf951ccc21b4f555327e44ab14fde8becee5
Kiko <kristiqnenchevv@gmail.com>
2024-11-21 11:43:50 +0200

    Create Stock Dto

---
+++ Server/Infrastructure/Models/Stock/StockDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Models.Stock
+{
+    using Domain.Entities;
+
+    public class StockDto : BaseAuditableDto<StockDto, Stock>
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public decimal CurrentPrice { get; set; }
+        public decimal DayHigh { get; set; }
+        public decimal DayLow { get; set; }
+        public decimal OpenPrice { get; set; }
+        public decimal PreviousClose { get; set; }
+        public long Volume { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
