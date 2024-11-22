7de8f253dfff1cd34492ef32b9158d3381364887
Kiko <kristiqnenchevv@gmail.com>
2024-11-22 10:37:35 +0200

    Craete watchlist item request model

---
+++ Server/Infrastructure/Models/Stock/CreateWatchlistItemRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Models.Stock
+{
+    public class CreateWatchlistItemRequest
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public decimal? AlertAbove { get; set; }
+        public decimal? AlertBelow { get; set; }
+    }
+}
