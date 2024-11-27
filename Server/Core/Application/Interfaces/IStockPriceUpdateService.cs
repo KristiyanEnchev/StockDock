95056346699e2fe5fa6015cf605fd743a863faed
Kiko <kristiqnenchevv@gmail.com>
2024-11-27 11:13:54 +0200

    Stock price updater service

---
+++ Server/Core/Application/Interfaces/IStockPriceUpdateService.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Interfaces
+{
+    public interface IStockPriceUpdateService
+    {
+        Task StartMonitoringAsync(CancellationToken cancellationToken);
+        Task StopMonitoringAsync();
+        Task UpdatePriceAsync(string symbol, decimal newPrice);
+        Task<IReadOnlyList<string>> GetActiveSymbolsAsync();
+    }
+}
