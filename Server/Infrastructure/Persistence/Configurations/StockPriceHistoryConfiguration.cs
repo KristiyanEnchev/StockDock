20bf37c8ca09c09f81ae71dc034a23d690d2907f
Kiko <kristiqnenchevv@gmail.com>
2024-11-28 13:07:34 +0200

    Create stock price history configuration

---
+++ Server/Infrastructure/Persistence/Configurations/StockPriceHistoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace Persistence.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Domain.Entities;
+
+    public class StockPriceHistoryConfiguration : IEntityTypeConfiguration<StockPriceHistory>
+    {
+        public void Configure(EntityTypeBuilder<StockPriceHistory> builder)
+        {
+            builder.ToTable("stock_price_histories");
+
+            builder.Property(h => h.Price)
+                .HasPrecision(18, 6)
+                .IsRequired();
+
+            builder.Property(h => h.Timestamp)
+                .IsRequired();
+
+            builder.HasIndex(h => new { h.StockId, h.Timestamp });
+        }
+    }
+}
