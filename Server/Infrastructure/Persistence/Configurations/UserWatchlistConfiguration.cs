d209d13a91c9d06c199fa382376158b2b356ef49
Kiko <kristiqnenchevv@gmail.com>
2024-11-29 11:08:59 +0200

    Create the user watchlist configuration

---
+++ Server/Infrastructure/Persistence/Configurations/UserWatchlistConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace Persistence.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Domain.Entities;
+
+    public class UserWatchlistConfiguration : IEntityTypeConfiguration<UserWatchlist>
+    {
+        public void Configure(EntityTypeBuilder<UserWatchlist> builder)
+        {
+            builder.ToTable("user_watchlists");
+
+            builder.Property(w => w.AlertAbove)
+                .HasPrecision(18, 6);
+
+            builder.Property(w => w.AlertBelow)
+                .HasPrecision(18, 6);
+
+            builder.HasIndex(w => new { w.UserId, w.StockId })
+                .IsUnique();
+
+            builder.HasOne(w => w.User)
+                .WithMany()
+                .HasForeignKey(w => w.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
