e85e7a0762277c4f348b420c1e9de248d01d960e
Kiko <kristiqnenchevv@gmail.com>
2024-12-05 14:34:38 +0200

    Get stock by symbol query

---
+++ Server/Core/Application/Handlers/Stocks/Queries/GetStockBySymbolQuery.cs
@@ -0,0 +1,41 @@
+﻿namespace Application.Handlers.Stocks.Queries
+{
+    using Microsoft.Extensions.Logging;
+
+    using MediatR;
+
+    using Application.Interfaces;
+
+    using Models.Stock;
+
+    using Shared;
+
+    public record GetStockBySymbolQuery(string Symbol) : IRequest<Result<StockDto>>;
+
+    public class GetStockBySymbolQueryHandler : IRequestHandler<GetStockBySymbolQuery, Result<StockDto>>
+    {
+        private readonly IStockService _stockService;
+        private readonly ILogger<GetStockBySymbolQueryHandler> _logger;
+
+        public GetStockBySymbolQueryHandler(
+            IStockService stockService,
+            ILogger<GetStockBySymbolQueryHandler> logger)
+        {
+            _stockService = stockService;
+            _logger = logger;
+        }
+
+        public async Task<Result<StockDto>> Handle(GetStockBySymbolQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await _stockService.GetStockBySymbolAsync(request.Symbol);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting stock for symbol {Symbol}", request.Symbol);
+                throw;
+            }
+        }
+    }
+}
