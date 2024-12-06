c6db2685ca9606dda5def8cf0bdd5e2794a9a1f4
Kiko <kristiqnenchevv@gmail.com>
2024-12-06 12:58:16 +0200

    Get user watchlist query

---
+++ Server/Core/Application/Handlers/Stocks/Queries/GetUserWatchlistQuery.cs
@@ -0,0 +1,43 @@
+﻿namespace Application.Handlers.Stocks.Queries
+{
+    using Microsoft.Extensions.Logging;
+
+    using MediatR;
+
+    using Application.Interfaces;
+
+    using Models.Stock;
+
+    using Shared;
+
+    public record GetUserWatchlistQuery(string UserId) : IRequest<Result<IReadOnlyList<UserWatchlistDto>>>;
+
+    public class GetUserWatchlistQueryHandler : IRequestHandler<GetUserWatchlistQuery, Result<IReadOnlyList<UserWatchlistDto>>>
+    {
+        private readonly IWatchlistService _watchlistService;
+        private readonly ILogger<GetUserWatchlistQueryHandler> _logger;
+
+        public GetUserWatchlistQueryHandler(
+            IWatchlistService watchlistService,
+            ILogger<GetUserWatchlistQueryHandler> logger)
+        {
+            _watchlistService = watchlistService;
+            _logger = logger;
+        }
+
+        public async Task<Result<IReadOnlyList<UserWatchlistDto>>> Handle(
+            GetUserWatchlistQuery request,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await _watchlistService.GetUserWatchlistAsync(request.UserId);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting watchlist for user {UserId}", request.UserId);
+                throw;
+            }
+        }
+    }
+}
