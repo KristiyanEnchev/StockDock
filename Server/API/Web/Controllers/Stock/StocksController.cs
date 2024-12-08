9e25da28cec362cefe902822d36a95465402951f
Kiko <kristiqnenchevv@gmail.com>
2024-12-08 11:24:25 +0200

    Create stock controller and watchlist conrller

---
+++ Server/API/Web/Controllers/Stock/StocksController.cs
@@ -0,0 +1,35 @@
+﻿namespace Web.Controllers.Stock
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    using Models.Stock;
+
+    using Shared;
+
+    using Web.Extensions;
+
+    using Application.Handlers.Stocks.Commands;
+    using Application.Handlers.Stocks.Queries;
+
+    [Authorize]
+    public class StocksController : ApiController
+    {
+        [HttpGet("{symbol}")]
+        public async Task<ActionResult<Result<StockDto>>> GetBySymbol(string symbol)
+        {
+            return await Mediator.Send(new GetStockBySymbolQuery(symbol))
+                .ToActionResult();
+        }
+
+        [HttpPut("{symbol}/price")]
+        [Authorize(Roles = "Administrator")]
+        public async Task<ActionResult<Result<StockDto>>> UpdatePrice(
+            string symbol,
+            [FromBody] decimal newPrice)
+        {
+            return await Mediator.Send(new UpdateStockPriceCommand(symbol, newPrice))
+                .ToActionResult();
+        }
+    }
+}
---
+++ Server/API/Web/Controllers/Stock/WatchlistController.cs
@@ -0,0 +1,49 @@
+﻿namespace Web.Controllers.Stock
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    using Application.Interfaces;
+
+    using Models.Stock;
+
+    using Shared;
+
+    using Web.Extensions;
+
+    using Application.Handlers.Stocks.Commands;
+    using Application.Handlers.Stocks.Queries;
+
+    [Authorize]
+    public class WatchlistController : ApiController
+    {
+        private readonly IUser _currentUser;
+
+        public WatchlistController(IUser currentUser)
+        {
+            _currentUser = currentUser;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Result<IReadOnlyList<UserWatchlistDto>>>> GetMyWatchlist()
+        {
+            return await Mediator.Send(new GetUserWatchlistQuery(_currentUser.Id!))
+                .ToActionResult();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Result<UserWatchlistDto>>> AddToWatchlist(
+            [FromBody] CreateWatchlistItemRequest request)
+        {
+            return await Mediator.Send(new AddToWatchlistCommand(_currentUser.Id!, request))
+                .ToActionResult();
+        }
+
+        [HttpDelete("{symbol}")]
+        public async Task<ActionResult<Result<bool>>> RemoveFromWatchlist(string symbol)
+        {
+            return await Mediator.Send(new RemoveFromWatchlistCommand(_currentUser.Id!, symbol))
+                .ToActionResult();
+        }
+    }
+}
