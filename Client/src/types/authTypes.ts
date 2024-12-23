b591c66efc47693cd715ed650c7c1b85a674312a
Kiko <kristiqnenchevv@gmail.com>
2024-12-23 14:54:05 +0200

    Update Auth types with token decoded values and Auth stathe

--- Client/src/types/authTypes.ts
+++ Client/src/types/authTypes.ts
@@ -16,3 +16,34 @@ export interface RegisterRequest {
     password: string;
     confirmPassword: string;
 }
+
+export interface DecodedToken {
+    Id: string;
+    Email: string;
+    FirstName: string;
+    LastName: string;
+    Username: string;
+    ip: string;
+    roles: string;
+    exp: number;
+    iss: string;
+    aud: string;
+}
+
+export interface User {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    roles: string[];
+}
+
+export interface AuthState {
+    user: User | null;
+    token: string | null;
+    refreshToken: string | null;
+    refreshTokenExpiryTime: string | null;
+    isAuthenticated: boolean;
+    isLoading: boolean;
+}
