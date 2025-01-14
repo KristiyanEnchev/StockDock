ff5e8541afbe35d4e437a60590f9b08341289c23
Kiko <kristiqnenchevv@gmail.com>
2025-01-14 15:26:54 +0200

    Create watchlist api

---
+++ Client/src/features/watchlist/watchlistApi.ts
@@ -0,0 +1,35 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { StockDto } from '../../types/stockTypes';
+import { baseQueryWithReauth } from '../api/baseQueryWithReauth';
+
+export const watchlistApi = createApi({
+    reducerPath: 'watchlistApi',
+    baseQuery: baseQueryWithReauth,
+    tagTypes: ['Watchlist'],
+    endpoints: (builder) => ({
+        getWatchlist: builder.query<StockDto[], void>({
+            query: () => 'watchlist',
+            providesTags: ['Watchlist']
+        }),
+        addToWatchlist: builder.mutation<void, string>({
+            query: (symbol) => ({
+                url: `watchlist/${symbol}`,
+                method: 'POST'
+            }),
+            invalidatesTags: ['Watchlist']
+        }),
+        removeFromWatchlist: builder.mutation<void, string>({
+            query: (symbol) => ({
+                url: `watchlist/${symbol}`,
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['Watchlist']
+        })
+    })
+});
+
+export const {
+    useGetWatchlistQuery,
+    useAddToWatchlistMutation,
+    useRemoveFromWatchlistMutation
+} = watchlistApi;
