c75053cd5709bbfd814dbbf1cdda5c69e64a19ce
Kiko <kristiqnenchevv@gmail.com>
2025-02-16 12:45:45 +0200

    Update Main layout

--- Client/src/components/layout/MainLayout.tsx
+++ Client/src/components/layout/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Footer } from '../common/Footer';
 
 export function MainLayout() {
     return (
-        <div className="min-h-screen flex flex-col bg-library-background dark:bg-library-dark-background">
+        <div className="min-h-screen flex flex-col bg-light-bg dark:bg-dark-bg">
             <Navigation />
             <main className="flex-grow pt-16">
                 <Outlet />
