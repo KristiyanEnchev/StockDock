dfa6e3e7c122ee4c75d7c89acfc4f8ced7e6191d
Kiko <kristiqnenchevv@gmail.com>
2025-02-25 15:24:57 +0200

    Update To Action result patern

--- Server/API/Web/Extensions/ResultExtensions.cs
+++ Server/API/Web/Extensions/ResultExtensions.cs
@@ -12,36 +12,31 @@
         {
             var result = await resultTask;
 
-            var response = new
-            {
-                success = result.Success,
-                data = result.Data,
-                errors = !result.Success ? result.Errors : null
-            };
-
             if (!result.Success)
             {
-                if (result.Errors != null)
+                if (result.Errors != null && result.Errors.Any())
                 {
-                    return new BadRequestObjectResult(new { Errors = response.errors });
+                    return new BadRequestObjectResult(new { errors = result.Errors });
                 }
-                return new BadRequestObjectResult("Unexpected Error occured");
+                return new BadRequestObjectResult("Unexpected Error occurred");
             }
 
-            else if (result.Data == null)
+            if (result.Data == null)
             {
                 return new NoContentResult();
             }
 
-            if (response.data is string)
+            if (result.Data is PaginatedResult<object> paginatedResult)
             {
-                return new OkObjectResult(new
-                {
-                    message = result.Data,
-                });
+                return new OkObjectResult(paginatedResult);
             }
 
-            return new OkObjectResult(response.data);
+            if (result.Data is string)
+            {
+                return new OkObjectResult(new { message = result.Data });
+            }
+
+            return new OkObjectResult(result.Data);
         }
     }
 }
